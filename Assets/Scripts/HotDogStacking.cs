fa13b298da53839ffc4a8176b7869956a5201e71
ThisIsKaluwa <aseriousmail@aol.com>
2021-01-11 10:27:29 +0100

    Hotdog besteht jetzt aus zwei individuellen Brötchenhälften

--- Assets/Scripts/HotDogStacking.cs
+++ Assets/Scripts/HotDogStacking.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/* This script handles the construction of a hotdog */
 public class HotDogStacking : MonoBehaviour
 {
 
     public GameObject Bun;
     public GameObject Sausage;
+
+    public GameObject SecondBun;
 
     private bool isStacked = false;
   
@@ -15,7 +18,7 @@ public class HotDogStacking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         IsAllStackedScript.howManyThingsToStack ++;
+         IsAllStackedScript.howManyThingsToStack ++; //add one to the total of items needed to be stacked
     }
 
     // Update is called once per frame
@@ -23,7 +26,7 @@ public class HotDogStacking : MonoBehaviour
     {
         
 
-        if (Vector3.Distance(Bun.transform.position, Sausage.transform.position) <= 0.03f && !isStacked)
+        if (Vector3.Distance(Bun.transform.position, Sausage.transform.position) <= 0.03f && Vector3.Distance(SecondBun.transform.position, Sausage.transform.position) <= 0.03f && !isStacked)
         {
             IsAllStackedScript.howManyThingsAreStacked++;
             Bun.GetComponent<Rigidbody>().isKinematic = true;
